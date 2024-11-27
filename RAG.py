255fdbaa6cbcfbcf6a43e2b050708bbc31ab4c7d
vinkolin363 <vinkolin363@gmail.com>
2024-11-27 15:18:48 +0500

    1. Проведён рефакторинг кода в файлах app.py и RAG.py. 2. Расширена функциональность интерфейса.
    TODO:
    1. Провести рефакторинг кода, вынести логику в функции.

--- RAG.py
+++ RAG.py
@@ -1,6 +1,6 @@
 import os
-import chromadb
 
+import chromadb
 from dotenv import load_dotenv
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
@@ -11,10 +11,9 @@ from langchain_community.vectorstores import Chroma
 from langchain_ollama import OllamaEmbeddings
 from langchain_ollama.llms import OllamaLLM
 
-load_dotenv('.env')
-
-OLLAMA_HOST = os.getenv('OLLAMA_HOST')
-OLLAMA_MODEL = os.getenv('OLLAMA_MODEL')
+load_dotenv(".env")
+OLLAMA_HOST = os.getenv("OLLAMA_HOST")
+OLLAMA_MODEL = os.getenv("OLLAMA_MODEL")
 
 llm = OllamaLLM(
     base_url=OLLAMA_HOST,
@@ -23,7 +22,6 @@ llm = OllamaLLM(
 
 md_directory = "data/"
 md_files = [f for f in os.listdir(md_directory) if f.endswith('.md')]
-
 print(f"Markdown files: {md_files}")
 
 text_splitter = CharacterTextSplitter(
@@ -35,13 +33,14 @@ text_splitter = CharacterTextSplitter(
 )
 
 docs = []
+
 for md_file in md_files:
     md_path = os.path.join(md_directory, md_file)
     loader = UnstructuredMarkdownLoader(md_path)
     pages = loader.load_and_split(text_splitter)
     docs.extend(pages)
-
     print(f"Content of {md_file}:")
+
     for page in pages:
         print(page)
 
@@ -49,15 +48,11 @@ for md_file in md_files:
     print(f"Loaded {len(pages)} pages from {md_file}")
 
 print(f"Total number of pages loaded: {len(docs)}")
-
 embeddings_model = OllamaEmbeddings(model="nomic-embed-text:latest")
-
 client = chromadb.HttpClient(host="http://localhost:8000")
 collection = client.get_or_create_collection(name="math_vector")
-
 db = Chroma.from_documents(docs, embedding=embeddings_model, client=client)
 db.add_documents(docs, collection=collection)
-
 retriever = db.as_retriever(search_kwargs={"k": 3})
 
 template = """
@@ -77,12 +72,8 @@ prompt = PromptTemplate.from_template(template)
 combine_docs_chain = create_stuff_documents_chain(llm, prompt)
 retrieval_chain = create_retrieval_chain(retriever, combine_docs_chain)
 
-def get_response(query: str) -> str:
-    """
-    Функция, возвращающая ответ на вопрос, основанный на текстовых данных.
 
-    :param query: текст вопроса
-    :return: текст ответа
-    """
+def get_response(query: str) -> str:
     response = retrieval_chain.invoke({"input": query})
+
     return response["answer"]
--- app.py
+++ app.py
@@ -1,21 +1,91 @@
 import streamlit as st
 from dotenv import load_dotenv
+
 from RAG import get_response
 
 load_dotenv(".env")
 
+st.set_page_config(
+    page_title="MathQuizGenerator",
+    page_icon="🏛",
+    layout="centered"
+)
 
-st.set_page_config(page_title="MathQuizGenerator", page_icon="🏛", layout="centered")
+if "mode" not in st.session_state:
+    st.session_state.mode = None
+if "user_input" not in st.session_state:
+    st.session_state.user_input = ""
+if "show_answer" not in st.session_state:
+    st.session_state.show_answer = False
+if "generated_answer" not in st.session_state:
+    st.session_state.generated_answer = ""
+if "is_loading" not in st.session_state:
+    st.session_state.is_loading = False
 
 st.title("RAG-система для математических заданий")
 
-st.write("Напишите вопрос на основе заданного контекста:")
+if st.session_state.mode is None:
+    st.subheader("Пожалуйста, выберите действие ниже")
+    col1, col2 = st.columns(2)
 
-user_input = st.text_area("Введите свой промпт...", height=100)
+    with col1:
+        if st.button("Найти определитель матрицы 2x2",
+                     key="matrix_determinant_2x2_button",
+                     use_container_width=True):
+            st.session_state.mode = "matrix_determinant_2x2"
+            st.rerun()
+    with col2:
+        if st.button("Свой запрос", key="custom_request_button", use_container_width=True):
+            st.session_state.mode = "custom_request"
+            st.rerun()
 
-if st.button("Получить ответ"):
-    if user_input:
-        answer = get_response(user_input)
-        st.markdown(f"### Ответ: {answer}")
-    else:
-        st.warning("Пожалуйста, введите вопрос.")
+elif st.session_state.mode == "matrix_determinant_2x2":
+    st.subheader("Вычисление определителя матрицы 2x2")
+    st.write("Функция находится в разработке...")
+
+    if st.button("Вернуться к выбору режима", key="back_button_matrix"):
+        st.session_state.mode = None
+        st.rerun()
+
+elif st.session_state.mode == "custom_request":
+    st.write("Напишите вопрос на основе заданного контекста.")
+
+    st.session_state.user_input = st.text_area(
+        "Введите свой промпт ниже",
+        value=st.session_state.user_input,
+        height=300
+    )
+
+    if st.session_state.is_loading:
+        st.spinner("Генерация ответа...")
+
+    is_disabled = st.session_state.is_loading
+
+    if st.button("Получить ответ", type="primary", key="generate_answer_button", disabled=is_disabled):
+        if st.session_state.user_input:
+            st.session_state.show_answer = False
+            st.session_state.is_loading = True
+            st.rerun()
+
+    if st.session_state.is_loading and not st.session_state.show_answer:
+        with st.spinner("Генерация ответа..."):
+            try:
+                answer = get_response(st.session_state.user_input)
+                st.session_state.show_answer = True
+                st.session_state.generated_answer = answer
+            except Exception as error:
+                st.error(f"Ошибка при генерации ответа: {error}")
+            finally:
+                st.session_state.is_loading = False
+                st.rerun()
+
+    if st.session_state.show_answer:
+        st.markdown("### **Ответ:**")
+        st.markdown(f"{st.session_state.generated_answer}")
+
+    if st.button("Вернуться к выбору режима", key="back_button_request"):
+        st.session_state.mode = None
+        st.session_state.show_answer = False
+        st.session_state.user_input = ""
+        st.session_state.generated_answer = ""
+        st.rerun()
